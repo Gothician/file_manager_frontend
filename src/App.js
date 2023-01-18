33fbaff4e19d8bd28e53769c65ae054a0e2dfc54
alextetero <alextetero@gmail.com>
2023-01-19 00:29:03 +0200

    Realized GET request to server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import { useEffect, useState } from 'react';
+import { IoFolderOutline } from 'react-icons/io5';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState({
+    path: '',
+    files: [],
+  });
+
+  const folderClickHadler = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:8000${e.target.attributes.href.value}`)
+      .then((res) => res.json())
+      .then(
+        (response) => {
+          // setParent('');
+          setData(response);
+        },
+        (err) => {}
+      );
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:8000/')
+      .then((res) => res.json())
+      .then(
+        (response) => {
+          // setParent('');
+          setData(response);
+        },
+        (err) => {}
+      );
+  }, []);
+
+  return (
+    <div className="file-manager">
+      <ul className="folder-list">
+        {data?.files.map((file) => {
+          if (file.dir) {
+            return (
+              <li key={file.name} className="list-item dir">
+                <a
+                  href={data.path + '/' + file.name}
+                  alt={`${file.name}`}
+                  onClick={folderClickHadler}
+                >
+                  <IoFolderOutline /> {file.name}
+                </a>{' '}
+              </li>
+            );
+          } else {
+            return (
+              <li key={file.name} className="list-item file">
+                {file.name}
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
